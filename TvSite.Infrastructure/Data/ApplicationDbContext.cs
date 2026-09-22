484619564c4d041e6bf4770bebca79b9e83600f4
JohanH <jojo2001@hotmail.se>
2026-09-22 10:53:28 +0200

    Refactor Infrastructure related objects from presentation to infrastructure

--- TvSite.Infrastructure/Data/ApplicationDbContext.cs
+++ TvSite.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace TvSite.Presentation.Data;
+namespace TvSite.Infrastructure.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
--- TvSite.Infrastructure/Data/ApplicationUser.cs
+++ TvSite.Infrastructure/Data/ApplicationUser.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace TvSite.Presentation.Data;
+namespace TvSite.Infrastructure.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
