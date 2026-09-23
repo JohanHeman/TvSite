24202cf60c0765650943fa73286cb6a28319480b
Alexandra <alexandra.westas1999@hotmail.com>
2026-09-23 14:34:15 +0200

    Add MediaEntity

---
+++ TvSite.Domain/Entities/MediaEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TvSite.Domain.Entities
+{
+    public class MediaEntity
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string[] Directors { get; set; }
+        public string[] Actors { get; set; }
+        public DateOnly AirDate { get; set; }
+        public string MediaImage { get; set; }
+    }
+}
