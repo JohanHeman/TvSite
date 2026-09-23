537b06933b2119b3d6cf033a0a9ac62ba30aaabc
Alex Hammarstedt <alexhammarstedt1998@gmail.com>
2026-09-23 15:23:21 +0200

    Add IcommentRepository

---
+++ TvSite.Domain/Interfaces/Repositories/ICommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using TvSite.Domain.Entities;
+
+namespace TvSite.Domain.Interfaces.Repositories;
+
+public interface ICommentRepository
+{
+    // Get comments
+    public Task GetCommentByIdAsync(string commentId);
+    public Task GetCommentsByMediaIdAsync(string mediaId);
+    public Task GetCommentsByUserIdAsync(string userId);
+
+    // Create update delete
+    public Task CreateCommentAsync(Comment comment);
+    public Task UpdateCommentAsync(Comment comment);
+    public Task DeleteCommentAsync(Comment comment);
+    
+}
