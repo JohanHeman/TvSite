220cebafaaad76b7dd3397d1eb486fe6a329d444
JohanH <jojo2001@hotmail.se>
2026-09-25 10:17:27 +0200

    Create first API interfaces

--- TvSite.Domain/InterfacesAPI/Repositories/IAPIRepository.cs
+++ TvSite.Domain/InterfacesAPI/Repositories/IAPIRepository.cs
@@ -1,6 +1,8 @@
+using TvSite.Domain.Entities;
+
 namespace TvSite.Domain.InterfacesAPI.Repositories;
 
 public interface IAPIRepository
 {
-    
+    public Task<List<Media>> GetMediasByTitle(string title);
 }
--- TvSite.Domain/InterfacesAPI/Services/IAPIService.cs
+++ TvSite.Domain/InterfacesAPI/Services/IAPIService.cs
@@ -1,6 +1,8 @@
+using TvSite.Domain.Entities;
+
 namespace TvSite.Domain.InterfacesAPI.Services;
 
 public interface IAPIService
 {
-    
+    public Task<List<Media>> GetMediasByTitle(string title);
 }
